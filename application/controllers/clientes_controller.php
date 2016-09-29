bc64904c413de0310f0a9c5ae95b21ec3665a283
Alder Hernandez <alderhernandez2009@hotmail.com>
2016-09-29 15:12:55 -0600

    V 1.0.4 - Se programo vista de usuarios al sistema. - Se programo vista de clientes al sistema de puntos (funcionalidadad al 80%) y exportacion a pdf - correccion de hoja de estilos. - se programo busqueda con input personalizado

--- application/controllers/clientes_controller.php
+++ application/controllers/clientes_controller.php
@@ -6,6 +6,7 @@ class Clientes_controller extends CI_Controller
     public function __construct(){
         parent::__construct();
         $this->load->library('session');
+        
         if($this->session->userdata('logged')==0){ //No aceptar a usuarios sin loguearse
             redirect(base_url().'index.php/login','refresh');
         }
